3fca9ae1f32f246d57d9aaafb7ac6e28a63e2eca
carlosperales95 <carlos.perales95@opendeusto.es>
2023-03-05 00:45:47 +0100

    Skeleton of demo component

---
+++ hacker-title/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="blob"></div>
+  <div id="blur"></div>
+  <h1 data-value="COMPLEXITY">COMPLEXITY</h1>
+
+  <div id="links">
+  <div class="meta-link">  
+      <span class="label">Glow Effect</span>
+      <span class="dot">·</span>
+      <a class="source" href="https://www.poppr.be" target="_blank">
+      <i class="fa-solid fa-link"></i>
+      </a>
+      <span class="dot">·</span>
+      <a class="youtube" href="https://youtu.be/kySGqoU7X-s" target="_blank">
+      <i class="fa-brands fa-youtube"></i>
+      </a>
+  </div>
+
+  <div class="meta-link">  
+      <span class="label">Text Effect</span>
+      <span class="dot">·</span>
+      <a class="source" href="https://kprverse.com" target="_blank">
+      <i class="fa-solid fa-link"></i>
+      </a>
+      <span class="dot">·</span>
+      <a class="youtube" href="https://youtu.be/W5oawMJaXbU" target="_blank">
+      <i class="fa-brands fa-youtube"></i>
+      </a>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+
+  @import "https://fonts.googleapis.com";
+  @import "https://fonts.gstatic.com";
+  @import "https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Rubik:wght@400;500&display=swap";
+
+</style>
+
