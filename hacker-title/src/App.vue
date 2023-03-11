ed80207bc04bd25c128253773d5a1a7b35a51cba
carlosperales95 <carlos.perales95@opendeusto.es>
2023-03-11 22:13:48 +0100

    minor tweaks

--- hacker-title/src/App.vue
+++ hacker-title/src/App.vue
@@ -3,6 +3,9 @@
   <div id="blur"></div>
   <h1 data-value="COMPLEXITY">COMPLEXITY</h1>
 
+  <a id="source-link" class="link" href="https://github.com/carlosperales95" target="_blank">
+    <span class="roboto-mono">GitHub</span>
+  </a>
 </template>
 
 <script>
