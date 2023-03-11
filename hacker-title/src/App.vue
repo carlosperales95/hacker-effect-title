6686c8e1f9cef33f0ebd959aaa1a271908ed7b9f
carlosperales95 <carlos.perales95@opendeusto.es>
2023-03-11 22:04:27 +0100

    changed some styles

--- hacker-title/src/App.vue
+++ hacker-title/src/App.vue
@@ -3,31 +3,6 @@
   <div id="blur"></div>
   <h1 data-value="COMPLEXITY">COMPLEXITY</h1>
 
-  <div id="links">
-  <div class="meta-link">  
-      <span class="label">Glow Effect</span>
-      <span class="dot">·</span>
-      <a class="source" href="https://www.poppr.be" target="_blank">
-      <i class="fa-solid fa-link"></i>
-      </a>
-      <span class="dot">·</span>
-      <a class="youtube" href="https://youtu.be/kySGqoU7X-s" target="_blank">
-      <i class="fa-brands fa-youtube"></i>
-      </a>
-  </div>
-
-  <div class="meta-link">  
-      <span class="label">Text Effect</span>
-      <span class="dot">·</span>
-      <a class="source" href="https://kprverse.com" target="_blank">
-      <i class="fa-solid fa-link"></i>
-      </a>
-      <span class="dot">·</span>
-      <a class="youtube" href="https://youtu.be/W5oawMJaXbU" target="_blank">
-      <i class="fa-brands fa-youtube"></i>
-      </a>
-  </div>
-  </div>
 </template>
 
 <script>
@@ -35,7 +10,6 @@
 export default {
   name: 'App',
   components: {
-    // HelloWorld
   }
 }
 </script>
